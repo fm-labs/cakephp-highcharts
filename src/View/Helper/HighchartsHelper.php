b7a62aa30024016c125cfc29aaec506308dfb10f
fm-labs <flowmotionlabs@gmail.com>
2020-03-02 21:56:38 +0100

    Initial

---
+++ src/View/Helper/HighchartsHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Highcharts\View\Helper;
+
+use Cake\View\Helper;
+
+/**
+ * Highcharts Helper
+ *
+ * @property \Cake\View\Helper\HtmlHelper $Html
+ */
+class HighchartsHelper extends Helper
+{
+    protected $_defaultConfig = [
+        //'scriptUrl' => '//code.highcharts.com/highcharts.js',
+        'scriptUrl' => '/highcharts/lib/highcharts/6.1.1/highcharts.js',
+        'block' => true
+    ];
+
+    public $helpers = ['Html'];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize(array $config)
+    {
+        $this->Html->script($this->config('scriptUrl'), ['block' => $this->config('block')]);
+    }
+}
